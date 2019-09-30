eb58a4c00bdf998497c1ab919880ef69578f1b5b
Matt King <matthew.king@stfc.ac.uk>
2019-09-30 18:29:37 +0100

    connected the file.import and file.export options to the import yaml and export yaml functions in the run_parameter_controller. Settings are now loaded and saved to yaml format

--- controller/run_parameter_controller.py
+++ controller/run_parameter_controller.py
@@ -1,3 +1,4 @@
+import PyQt4.QtGui as QtGui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import run_parameters_parser as yaml_parser
@@ -52,58 +53,93 @@ class RunParameterController(QObject):
 
     def widget_names_in_layout(self, widget):
         if isinstance(widget, QLineEdit):
-            self.model.data.data_values.update({str(widget.objectName()): str(widget.text())})
+            self.model.data.data_values.update({str(widget.objectName()).replace('_line_edit',''): str(widget.text())})
         elif isinstance(widget, QCheckBox):
             if widget.isChecked():
-                self.model.data.data_values.update({str(widget.objectName()): 'T'})
+                self.model.data.data_values.update({str(widget.objectName()).replace('_check_box',''): 'T'})
             else:
-                self.model.data.data_values.update({str(widget.objectName()): 'F'})
+                self.model.data.data_values.update({str(widget.objectName()).replace('_check_box',''): 'F'})
         return
-	
-	def import_parameter_values_from_yaml_file(self):
-	    dialog = Qt.FileDialog()
-	    filename = Qt.QFileDialog.getOpenFileName(dialog, caption='Open file',
-                                                  directory='c:\\',
-											      filter="YAML files (*.YAML *.YML *.yaml *.yml)")	
-	    if not filename.isEmpty():
-		    loaded_parameter_list = yaml_parser.parse_parameter_input_file(filename)
-			line_edit_list = self.get_all_line_edits_in_main_window()
-			self.set_all_line_edits_in_main_window(line_edit_list,loaded_parameter_list)
-			# get all check boxes
-			# set all check box
-			
-			
-	def get_all_line_edits_in_main_window(self):
-		line_edit_list = []
-		main_window = self.ui.centralwidget
-		# gather all the children of the central widget
-		# which will be the gird layouts containing the line-edits
-		# and check-box widgets
-		grid_layouts = main_window.children()
-		for widget in grid_layouts:
-			if isinstance(widget, Qt.QLineEdit):
-				line_edit_list.append(widget)
-		return line_edit_list
+    ## Need to port this to the unified controller
+    def import_parameter_values_from_yaml_file(self):
+        dialog = QtGui.QFileDialog()
+        filename = QtGui.QFileDialog.getOpenFileName(dialog, caption='Open file',
+                                                     directory='c:\\',
+                                                     filter="YAML files (*.YAML *.YML *.yaml *.yml)")  
+        if not filename.isEmpty():
+            loaded_parameter_list = yaml_parser.parse_parameter_input_file(filename)
+            line_edit_list = self.get_all_line_edits_in_main_window()
+            self.set_all_line_edits_in_main_window(line_edit_list,loaded_parameter_list)
+            check_box_list = self.get_all_check_boxes_in_main_window()
+            self.set_all_check_boxes_in_main_window(check_box_list, loaded_parameter_list)
+        else:
+            print 'Failed to import, please provide a filename'
 
-    def get_all_check_boxes_in_main_window(self):
-        check_box_list = []
-        main_window = self.ui.centralwidget
+    def export_parameter_values_to_yaml_file(self):
+        dialog = QtGui.QFileDialog()
+        dialog.setFileMode(QtGui.QFileDialog.DirectoryOnly)
+        filename, _filter = QtGui.QFileDialog.getSaveFileNameAndFilter(dialog, caption='Save File', directory='c:\\',
+                                                                    filter="YAML Files (*.YAML *.YML *.yaml *.yml")
+        # we have a validation check in collect_line_edit_text() called by collect_parameters()
+        # so if a value isn't valid, that entry for the data.parameters is not set.
+        #
+        # Currently, this means that if we have an invalid input, the yaml file isn't written out.
+        # However, all of this is implicit. So need to find a way to explicitly validate for YAML exporting
+        if not filename.isEmpty():
+            self.collect_parameters()
+            # IF validate(parameter_values) THEN:
+            yaml_parser.write_parameter_output_file(filename, self.model.data.data_values)
+            # ELSE: RAISE EXCEPTION/DISPLAY INVALID INPUT TO USER
+        else:
+            print 'Failed to export, please provide a filename.'
+    ## Need to port this to the unified controller
+    def get_all_line_edits_in_main_window(self):
+        line_edit_list = []
+        main_window = self.view.centralwidget
         # gather all the children of the central widget
         # which will be the gird layouts containing the line-edits
         # and check-box widgets
-        grid_layouts = main_window.children()
+        grid_layouts = main_window.findChildren(QLineEdit)
         for widget in grid_layouts:
-            if isinstance(widget, Qt.QCheckBox):
+            if isinstance(widget, QLineEdit):
+                line_edit_list.append(widget)
+        return line_edit_list
+    ## Need to port this to the unified controller
+    def get_all_check_boxes_in_main_window(self):
+        check_box_list = []
+        main_window = self.view.centralwidget
+        # gather all the children of the central widget
+        # which will be the gird layouts containing the line-edits
+        # and check-box widgets
+        grid_layouts = main_window.findChildren(QCheckBox)
+        for widget in grid_layouts:
+            if isinstance(widget, QCheckBox):
                 check_box_list.append(widget)
-        return check_box_list	
-        @staticmethod
+        return check_box_list
+    ## Need to port this to the unified controller
+    @staticmethod
     def set_all_line_edits_in_main_window(line_edit_list, values_to_set_dict):
-        print values_to_set_dict
         for line_edit in line_edit_list:
             # our YAML values dict has keys without the '_line_edit' suffix, so we must add this suffix to
             # access the line_edit dict which has keys containing the '_line_edit' suffix.
-            parameter_value_str = str(values_to_set_dict[str(line_edit.objectName()).replace('_line_edit', '')])
+            print values_to_set_dict
+            line_edit_value_str = str(line_edit.objectName()).replace('_line_edit', '')
+            parameter_value_str = str(values_to_set_dict[line_edit_value_str])
             line_edit.setText(parameter_value_str)
+            print line_edit_value_str
+            print parameter_value_str
+    ## Need to port this to the unified controller
+    @staticmethod
+    def set_all_check_boxes_in_main_window(check_box_list, values_to_set_dict):
+        for check_box in check_box_list:
+            # our YAML values dict has keys without the '_check_box' suffix, so we must add this suffix to
+            # access the check_box_list dict which has keys containing the '_check_box' suffix.
+            parameter_value_str = str(values_to_set_dict[str(check_box.objectName()).replace('_check_box', '')])
+            # now we convert from T/F to Checked/Unchecked for the check-boxes
+            if parameter_value_str == 'T':
+                check_box.setChecked(True)
+            elif parameter_value_str == 'F':
+                check_box.setChecked(False)
 
     def disable_run_button(self):
         self.view.runButton.setEnabled(False)
@@ -130,5 +166,5 @@ class RunParameterController(QObject):
 
     @pyqtSlot()
     def handle_existent_file(self):
-        print('Directory '+self.model.data.data_values['directory_line_edit'] + 'already exists')
+        print('Directory '+self.model.data.data_values['directory'] + 'already exists')
 
