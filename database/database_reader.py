4bb3756a7d00e9dfdc0d3884077e4f562e4e1e18
Matt King <matthew.king@stfc.ac.uk>
2020-06-03 08:53:04 +0100

    trying to reconstruct settings dictionaries from database

--- database/database_reader.py
+++ database/database_reader.py
@@ -4,7 +4,7 @@ import uuid
 import run_parameters_parser as yaml_parser
 import collections
 import json
-
+from collections import defaultdict
 ## We can get rows back from SQL as a Row object
 ## This allows us to compare the yaml files and the DB entries more readily.
 
@@ -19,21 +19,37 @@ class DatabaseReader():
         self.kwargs = kwargs
         self.sql_connection = sqlite3.connect('SimulationDatabase.db')
         self.sql_cursor = self.sql_connection.cursor()
+        self.sql_connection.row_factory = sqlite3.Row
+        self.table_name_list = ["generator", "INJ", "EBT", "S02", "C2V", "BA1", "scan", "simulation"]
 
-
+    def get_unique_run_id_and_table_dict(self):
+        run_number_table_dict = defaultdict(list)
+        for table_name in self.table_name_list:
+            self.sql_cursor.execute('select DISTINCT run_id from ' + table_name)
+            unique_run_id = self.sql_cursor.fetchall()
+            if len(unique_run_id) > 0:
+                run_number_table_dict[table_name].append(unique_run_id[0][0])
+        return run_number_table_dict
 
 if __name__ == '__main__':
     db_creator = DatabaseReader()
-    db_creator.sql_connection.row_factory = sqlite3.Row
-    db_creator.sql_cursor.execute('select * from BA1')
-    row = db_creator.sql_cursor.fetchall()
-    column_names = list()
-    for index in range(0,4):
-        column_names.append(db_creator.sql_cursor.description[index][0])
-    print(column_names)
-    print(type(row))
-    print(len(row))
-    unique_entry_dict = dict()
-    for item in row:
-        print(item)
-    print(unique_entry_dict)
+    table_run_id_dict = db_creator.get_unique_run_id_and_table_dict()
+    print(table_run_id_dict)
+    run_id_settings_dict = defaultdict(list)
+    for table_name, unique_runs in table_run_id_dict.items():
+        for run_id in unique_runs:
+            db_creator.sql_cursor.execute('select * from ' + table_name + ' where run_id=\'' +run_id + '\'')
+            settings_for_run_id = db_creator.sql_cursor.fetchall()
+            run_id_settings_dict[run_id][table_name].append(settings_for_run_id[run_id])
+    print(run_id_settings_dict)
+    ##### Now recreate the settings dict per run_number:
+    ##### select * from table_name WHERE run_id = table_run_id_dict[table_name]
+    #### and split up row into the form: table_name : {parameter_name : value}
+    #### we can then have a full dictionary keyed by run_id: {run_id : {table_name : {parameter_name : value} } }
+    
+            # settings_entry_dict[table_name].append(tuple(item[1:]))
+            # print(item)
+    # run_entry_dict[row[0]] = settings_entry_dict
+    # for key, value in run_entry_dict.items():
+            # print("KEY: ", key, " VALUE: ", value, "\n")
+            # print("")
