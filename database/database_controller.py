c101f57dd16c30e7f53200cab89bc1f010a270aa
Matt King <matthew.king@stfc.ac.uk>
2020-06-11 17:25:19 +0100

    added __main__ function to give example of use

--- database/database_controller.py
+++ database/database_controller.py
@@ -1,4 +1,4 @@
-import run_parameters_parse as yaml_parser
+import run_parameters_parser as yaml_parser
 import database_reader 
 import database_writer
 
@@ -9,7 +9,7 @@ class DatabaseController():
         self.writer = database_writer.DatabaseWriter()
         self.settings_dict = None
     ## NEED TO CHECK IF YAML_DICT HAS BEEN SET, OTHERWISE USE ARGUMENT.
-    def are_settings_in_db(self, settings_to_save=None):
+    def are_settings_in_database(self, settings_to_save=None):
         if settings_to_save == None:
             if self.settings_dict != None:
                 print('Checking YAML settings against database...')
@@ -24,15 +24,27 @@ class DatabaseController():
     def get_run_id_for_settings(self, settings_to_save=None):   
         if settings_to_save == None:
             if self.settings_dict != None:
-                print('Checking YAML settings against database...')
+                print('Searching for RUN ID for YAML settings...')
                 settings_to_check = self.reader.prepare_dict_for_checking(self.settings_dict)
-                return get_run_id_for_settings(settings_to_check)
+                return self.reader.get_run_id_for_settings(settings_to_check)
             else:
                 print('Supply a settings dictionary, or load yaml settings before checking database')
         else:
             print('Checking dictionary against database...')
             settings_to_check = self.reader.prepare_dict_for_checking(settings_to_save)
-            return get_run_id_for_settings(settings_to_check)
+            return self.reader.get_run_id_for_settings(settings_to_check)
             
     def load_yaml_settings(self, yaml_filename):
         self.settings_dict = yaml_parser.parse_parameter_input_file(yaml_filename)
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    dbc = DatabaseController()
+    dbc.load_yaml_settings('scan_settings.yaml')
+    if dbc.are_settings_in_database():
+        print(dbc.get_run_id_for_settings())
+    else:
+        print('FAILURE;')
